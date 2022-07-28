43b036f882cf2936bf1d59fabb91cc16dd5cf594
Khali Gopaul <you@example.com>
2022-07-28 15:14:23 -0400

    made a cloud function

---
+++ functions/index.js
@@ -0,0 +1,16 @@
+const { app } = require("firebase-admin");
+const functions = require("firebase-functions");
+
+// app.get('/helloThere', )
+
+exports.helloThere = functions.https.onRequest((req, res) => {
+    res.send('Hello there Firebase Cloud user!');
+})
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
